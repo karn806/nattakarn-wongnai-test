21a90462242b827cc4acd416dc7c002bfaa08e3a
karn806 <nattakarn.kl@gmail.com>
2019-04-11 16:30:55 +0700

    throw error

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 class Store {
 	constructor(reducer, initialState) {
+		this.listeners = []
 		this.reducer = reducer
 		this.state = initialState
 	}
@@ -15,20 +16,37 @@ class Store {
 			default:
 				const newState = this.reducer(this.state, action)
 				this.state = newState
+				this.listeners.slice().forEach(listener => listener());
 		}
 	}
 
 	subscribe(listener) {
-		
+		if (typeof listener !== 'function') {
+	    throw new Error('Expected the listener to be a function.')
+	  } else {
+			this.listeners.push(listener);
+		  return function unsubscribe() {
+		    var index = this.listeners.indexOf(listener);
+		    this.listeners.splice(index, 1);
+		  }
+		}
 	}
 
 	replaceReducer(nextReducer) {
-		this.reducer = nextReducer
+		if (typeof nextReducer !== 'function') {
+	    throw new Error('Expected the reducer to be a function.')
+	  } else {
+			this.reducer = nextReducer
+		}
 	}
 }
 
 function createStore(reducer, initialState) {
-	return new Store(reducer, initialState)
+	if (typeof reducer !== 'function') {
+		throw new Error('Expected the reducer to be a function.')
+	} else {
+			return new Store(reducer, initialState)
+	}
 }
 
 exports.createStore = createStore
